97deff1979e9d1f475c0d6b58cd2b7a1c0fe576d
vegvisir <alexandr.rostow@gmail.com>
2021-11-07 12:20:26 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
 'use strict';
+//__устаревшие методы
+// const box = document.getElementById('box'); // конкретный элемент
+// console.log(box); 
 
-const box = document.getElementById('box');
+// const btns = document.getElementsByTagName('button'); // массив элементов по имени тэга
+// console.log(btns[2]); // третий элемент массива
 
-console.log(box);
-
-
-
-
-
-
-
+// const circles = document.getElementsByClassName('circle'); // массив элементов по имени класса
+// console.log(circles);
 
 
+//__новые методы
+const hearts = document.querySelectorAll('.heart');
+// console.log(hearts);
 
+hearts.forEach(item => { // item - каждый элемент массива hearts
+    console.log(item);
+});
 
+const oneHeart = document.querySelector('div');
+console.log(oneHeart);
 
 
 
